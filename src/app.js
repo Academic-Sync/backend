7b6203e578b68a15bb86c8fc677954d0decc68ea
joaoenrique <jebsantosalves@gmail.com>
2024-10-26 14:23:20 -0300

    feat: adicionar cors

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ const studentsRoutes = require('./routes/student');
 const teachersRoutes = require('./routes/teacher');
 const coordinatorsRoutes = require('./routes/coordinator');
 const auth = require('./middlewares/auth');
-
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://127.0.0.1:5500','http://localhost:5500', 'http://localhost:8080', 'http://127.0.0.1:8088'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY', 'X-Requested-With', 'X-Custom-Header'],
+    credentials: true,
+    optionsSuccessStatus: 204
+  };
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
