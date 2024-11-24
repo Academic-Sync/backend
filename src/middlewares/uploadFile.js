bc1d8de63ae22dcea6292e61fae4eca5dbefcf45
JoaoEnrique <joaoenriquealves@gmail.com>
2024-11-24 10:23:36 -0300

    fix: criar pasta de upload das atividades se não existir

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,12 +1,23 @@
 const multer = require('multer');
+const fs = require('fs');
 
+// Função para verificar e criar o diretório, se necessário
+const ensureDirectoryExistence = (directory) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+};
+
+// Configuração do armazenamento para o Multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads'); // Diretório onde os arquivos serão armazenados
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname); // Nome original do arquivo
-    }
+  destination: (req, file, cb) => {
+    const uploadDir = './uploads'; // Diretório onde os arquivos serão armazenados
+    ensureDirectoryExistence(uploadDir); // Garante que o diretório exista
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // Nome original do arquivo
+  },
 });
 
 const uploadFile = multer({ storage });
--- src/middlewares/uploadFileActivity.js
+++ src/middlewares/uploadFileActivity.js
@@ -1,23 +1,27 @@
-try {
-  const multer = require('multer');
-  const path = require('path');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-    // Configuração do armazenamento para o Multer
-    const storage = multer.diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, 'uploads/activity'); // Pasta onde os arquivos serão armazenados
-      },
-      filename: (req, file, cb) => {
-        const name = Date.now() + "----" + file.originalname;
-        
-        cb(null, name); // Nome único para o arquivo
-      },
-    });
+// Função para garantir que a pasta exista
+const ensureDirectoryExistence = (directory) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true }); // Cria a pasta, incluindo subpastas, se necessário
+  }
+};
 
-    const upload = multer({ storage });
+// Configuração do armazenamento para o Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadPath = 'uploads/activity'; // Pasta onde os arquivos serão armazenados
+    ensureDirectoryExistence(uploadPath); // Verifica e cria a pasta, se necessário
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const name = Date.now() + "----" + file.originalname;
+    cb(null, name); // Nome único para o arquivo
+  },
+});
 
-    module.exports = upload;
+const upload = multer({ storage });
 
-} catch (error) {
-  return res.status(500).json({error: error.message});
-}
+module.exports = upload;
